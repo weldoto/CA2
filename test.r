ac3d4abecf42aa2524eb8c34e8610c69a6c7f8d2
epa <epa@localhost.localdomain>
2017-12-12 16:58:17 +0000

    test added

---
+++ test.r
@@ -0,0 +1,7 @@
+age <- readline(prompt="Enter age: ")
+name <-readline(prompt="Enter you name: ")
+
+age <- as.integer(age)
+
+print(paste("Hi",name, "next year you will be " ,age+1, "yeasrs old"))
+
