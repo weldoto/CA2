b9bd8f0be5a596d46928444de671200099c29ae1
epa <epa@localhost.localdomain>
2017-12-14 13:23:31 +0000

    14Dec

--- func.r
+++ func.r
@@ -1,12 +1,21 @@
+#install.packages('data.table')
+library(parallel)
 library(data.table)
 stocks <-read.table('/home/epa/CA2/stocksNumeric.csv',header=T, sep=',')
+res <-mean(stocks$gain)
+print(res)
 rows <- readline(prompt="Enter rows: ")
 rows <- as.integer(rows)
 dt <-data.table(stocks)
 
 x=rows
 findrow <-function(){
-dt[,head(.SD,x), by = "stockNum"]
+avg<-dt[,head(.SD,x), by = "stockNum"]
+avgforG <-avg[,.(gain=mean(gain)), by =.(stockNum)]
+#print(avg)
+#print(avgforG)
+print(mean(avgforG$gain))
+#print(sum(avgforG$gain)/10)
 }
 print(findrow())
 
